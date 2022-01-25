310d935a896cdfdb93f3047a3198ef64bbe0dadf
Mattia Franchetto <mt.franchetto@gmail.com>
2022-01-25 14:25:27 +0100

    Remove Mas

--- pre-install.sh
+++ pre-install.sh
@@ -5,7 +5,5 @@
  git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow
  git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask fetch --unshallow
 
-brew install mas
-
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
